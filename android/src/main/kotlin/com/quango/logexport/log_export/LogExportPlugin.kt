a32540887ff7ca835c37763d2ce93fdbdce38aad
Tiberiu.Danciu <danciu.tiberiu@gmail.com>
2024-01-23 15:11:40 +0200

    Fix storage directory

--- android/src/main/kotlin/com/quango/logexport/log_export/LogExportPlugin.kt
+++ android/src/main/kotlin/com/quango/logexport/log_export/LogExportPlugin.kt
@@ -29,7 +29,7 @@ class LogExportPlugin: FlutterPlugin, MethodCallHandler {
 
   override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
     if (call.method == "getLogFileExportedPath") {
-      val filename = File(Environment.getExternalStoragePublicDirectory(
+      val filename = File(Environment.getDataDirectory(
               Environment.DIRECTORY_DOWNLOADS).toString() + "/logcat_export.txt")
       filename.createNewFile()
       val pid = android.os.Process.myPid();
